cd98af78cd37fb64898bb9cf2d53d64b05779eb3
JacobYoungT <jacobyounginvest@gmail.com>
2025-02-21 10:57:26 +0100

    Create scraper

---
+++ scrape_sp500.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+link = (
+    "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies#S&P_500_component_stocks"
+)
+df = pd.read_html(link, header=0)[0]
+
+# Write to .csv
+df.to_csv("data/sp500_tickers.csv", index=False)
