42624acc0f1e179f05b7a8824bc373182b1ae559
JacobYoungT <jacobyounginvest@gmail.com>
2025-02-21 17:28:13 +0100

    Updated scraper to only save the tickers in csv file

--- scrape_sp500.py
+++ scrape_sp500.py
@@ -4,6 +4,7 @@ link = (
     "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies#S&P_500_component_stocks"
 )
 df = pd.read_html(link, header=0)[0]
+df = df['Symbol']
 
 # Write to .csv
 df.to_csv("data/sp500_tickers.csv", index=False)
