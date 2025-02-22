5baf4b825150638c24141aed2027f711ddbd6cb5
JacobYoungT <jacobyounginvest@gmail.com>
2025-02-22 17:50:51 +0100

    project folder renamed to momentum_calculator

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from project.scrape_sp500 import scrape_sp500_tickers
-from project.calculator import calculate_momentum_and_smoothness
+from momentum_calculator.scrape_sp500 import scrape_sp500_tickers
+from momentum_calculator.calculator import calculate_momentum_and_smoothness
 
 def main():
     scrape_sp500_tickers()
